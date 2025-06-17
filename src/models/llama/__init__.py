35c9567ce7503c9456ec19a25800071c0b077b92
Kira Selby <kaselby@uwaterloo.ca>
2025-06-17 12:21:28 -0400

    Fixes for loading AutoConfig.from_pretrained
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>

--- src/models/llama/__init__.py
+++ src/models/llama/__init__.py
@@ -2,8 +2,8 @@ from . import configuration_llama_skip
 from . import modelling_llama_skip
 
 from transformers import AutoConfig, AutoModelForCausalLM
-from configuration_llama_skip import LlamaSkipConnectionConfig
-from modelling_llama_skip import LlamaSkipConnectionForCausalLM
+from .configuration_llama_skip import LlamaSkipConnectionConfig
+from .modelling_llama_skip import LlamaSkipConnectionForCausalLM
 AutoConfig.register("llama-skip", LlamaSkipConnectionConfig)
 AutoModelForCausalLM.register(LlamaSkipConnectionConfig, LlamaSkipConnectionForCausalLM)
 
