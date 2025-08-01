420db33715bdcb69e32e9c574a855a325c77ab62
Kira Selby <30674826+kaselby@users.noreply.github.com>
2025-08-01 15:16:35 -0400

    KV Cache and Topk sparsity (#61)
    * Add KV cache to benchmark.py
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Add KV cache to benchmark.py
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Fixes
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Add topk and statistical topk sparsity methods as well as initial sparsity thresholds
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Fix activation capture for generate dataset
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Fix activation capture for generate dataset
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Fix config sparsities
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Fix config
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Fix activation capture to be capturing hidden states at the start of layer instead of start of MLP block
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * Added documentation to measure_gt_sparsity to indicate how it can be used to calculate sparsity thresholds for topk
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    * add sparsity method parameter to downstream eval
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    
    ---------
    
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>
    Signed-off-by: Kira Selby <30674826+kaselby@users.noreply.github.com>

--- src/activation_capture.py
+++ src/activation_capture.py
@@ -25,7 +25,7 @@ class ActivationCapture():
             # Just detach, don't clone or move to CPU yet
             self.mlp_activations[Hook.IN][layer_idx] = input[0].clone().detach()
             return output
-        handle = layer.self_attn.register_forward_hook(hook)
+        handle = layer.register_forward_hook(hook)
         return handle
 
     def _register_act_hook(self, layer_idx, layer):
