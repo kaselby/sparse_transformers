546cbaf06e49987680e21793270494226a407919
Kira Selby <kaselby@uwaterloo.ca>
2025-08-08 09:27:32 -0400

    Refactor activation capture to work correctly with opt.
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>

--- src/activation_capture.py
+++ src/activation_capture.py
@@ -2,6 +2,7 @@
 from enum import Enum
 from typing import List
 
+
 class Hook(Enum):
     IN = "IN"
     ACT = "ACT"
@@ -31,7 +32,7 @@ class ActivationCapture():
     def _register_act_hook(self, layer_idx, layer):
         def hook(module, input, output):
             # Just detach, don't clone or move to CPU yet
-            self.mlp_activations[Hook.ACT][layer_idx] = input[0].clone().detach()
+            self.mlp_activations[Hook.ACT][layer_idx] = output[0].clone().detach()
             return output
         handle = layer.mlp.act_fn.register_forward_hook(hook)
         return handle
@@ -55,7 +56,7 @@ class ActivationCapture():
     def get_layers(self):
         return self.model.get_decoder().layers
 
-    def register_hooks(self, hooks=(Hook.ACT, Hook.UP, Hook.OUT)):
+    def register_hooks(self, hooks):
         """Register forward hooks to capture activations."""
         # Clear any existing hooks
         self.remove_hooks()
@@ -98,3 +99,62 @@ class ActivationCapture():
         self.mlp_activations = {
             hook: {} for hook in self.hooks_available
         }
+
+
+
+LOOKUP_DICT = {}
+
+def register(*model_types):
+    '''
+    A decorator to record model-specific activation capture implementations.
+    Arguments
+    ----------
+    *model_types: str
+          Variable-length list of model types to register the given implementation under.
+    Returns
+    ----------
+    callable:
+          Decorator function to register model implementation.
+    Examples
+    ----------
+    >>> @register('opt')
+    ... class ActivationCaptureOPT(ActivationCapture): # This class definition gets registered for opt
+    '''
+    def decorator(cls):
+        for model_type in model_types:
+            if model_type in LOOKUP_DICT:
+                raise LookupError(f"{model_type} already present") 
+            LOOKUP_DICT[model_type] = cls
+        return cls
+    return decorator
+
+def capture_model(model):
+    '''
+    Returns an ActivationCapture instance of the correct type for the given model using the 
+    lookup dict (or a default implementation if no model-specific implementation is found).
+    Arguments
+    ----------
+    model: PreTrainedModel
+          Model object to use as lookup key and argument to instantiate the activation capture.
+    Returns
+    ----------
+    ActivationCapture:
+          ActivationCapture implementation stored in registry for the provided model type 
+          (or default implementation as fallback if none found).
+    Examples
+    ----------
+    >>> @register('opt')
+    ... class ActivationCaptureOPT(ActivationCapture): # This class definition gets registered for opt
+    >>> model.activation_capture = capture_model(model) # Loads activation capture class from registry
+    >>> model.activation_capture.register_hooks()   # Use activation capture class to record model activations
+    __main__.DLRM_Net
+    '''
+    model_type = model.model_type
+    
+    # Use model-specific implementation if available, otherwise fall back to default
+    if model_type in LOOKUP_DICT:
+        act_cls = LOOKUP_DICT[model_type]
+    else:
+        act_cls = ActivationCapture
+    
+    return act_cls(model)
